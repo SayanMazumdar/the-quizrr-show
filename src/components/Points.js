014e3c22f669caebb3628a540fdda4e24ed91cba
SayanMazumdar <sayanmazumdar355@outlook.com>
2025-01-25 23:42:56 +0530

    Minor bug fixes

--- src/components/Points.js
+++ src/components/Points.js
@@ -5,15 +5,16 @@ import { useQuiz } from '../contexts/QuizContext';
 
 export default function Points() {
 
-    const { points, totalRes, dispatch, status, totalQues } = useQuiz();
-    
+    const { points, dispatch, status, totalQuestions, responses } = useQuiz();
+    const totalRes = responses.length;
+
     return (
         <>
             <div className='points'>
                 <img src={status === 'finish' ? `${Congrats}` : `${Timeup}`} alt="Congrats trophy" />
                 {status === 'timeUp' && <h4 style={{ color: 'var(--color-error)', marginBottom: '1rem' }}>Time is up!!</h4>}
                 <h3>You scored</h3>
-                <h2>{points} out of {totalQues * 10} points</h2>
+                <h2>{points} out of {totalQuestions * 10} points</h2>
                 {status === 'finish' && <h4>Quiz completed successfully!</h4>}
                 <p>You attempted <span style={{ color: 'var(--color-theme)' }}>{totalRes} {totalRes > 1 ? 'questions' : 'question'}</span> and from that <span style={{ color: 'var(--color-correct)' }}>{points / 10} {points / 10 > 1 ? 'answers were' : 'answer was'} correct</span></p>
             </div>
